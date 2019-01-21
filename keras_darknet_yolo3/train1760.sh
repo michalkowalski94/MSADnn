3ab0e9a14f6630e1d8c9d82d43775a334d474996
michalkowalski94 <michal.bozydar.kowalski@gmail.com>
2019-01-21 20:53:28 +0100

    added keras(tensorflow) darknet yolov3 for 1760x1760 rescaled images

---
+++ keras_darknet_yolo3/train1760.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+#SBATCH -A neuralcbisddsm
+#SBATCH -p plgrid-gpu
+#SBATCH --gres=gpu
+#SBATCH --time=72:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=8GB
+
+export LD_LIBRARY_PATH=/net/people/plgmkowalski94/cudnn/cuda/lib64:$LD_LIBRARY_PATH
+module load plgrid/apps/cuda/9.0
+module load plgrid/libs/tensorflow-gpu/1.10.0-python-3.6
+python3 $SCRATCH/master/new/keras-yolo3/1760train.py
